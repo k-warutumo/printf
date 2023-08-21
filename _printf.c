7da8dd4ee049e7a6be7ff6c1286d0c43ffb83305
k-warutumo <kevinwnderitu@gmail.com>
2023-08-21 14:20:44 -0700

    task0

--- _printf.c
+++ _printf.c
@@ -1,7 +1,37 @@
 #include "main.h"
+#include <stdarg.h>
+
 
 int _printf(const char *format, ...);
 {
-	int jambo = 0;
+	int jambo = 0;/*char to print*/
 
+	va_list(list_of_argu, format)
+	while(*format)
+		if(format =! '%')/*check if special char */
+		{
+			write(1, format, 1);
+				jambo++;
+		}
+		else/*move to the next char */
+		{
+			format++;
+		}
+		if(format == '\0')/*exclude the end byte as per instruction */
+		{
+			break;
+		}
+		if(format == c)
+		{
+			write(1, format, 1);
+		}
+		if(format == s)
+		{
+			write(1, format, 1);
+		}
+
+
+
+	va_start
+	va_arg	
 
